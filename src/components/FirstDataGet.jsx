5aeeb95db1aec5a1e423638689fe8a3d49708db9
bikashroy120 <bikashroydt@gmail.com>
2023-03-05 15:16:44 +0600

    data creact ,update ,delete get add

--- src/components/FirstDataGet.jsx
+++ src/components/FirstDataGet.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs,addDoc } from "firebase/firestore"; 
+import { collection, getDocs,addDoc, doc, deleteDoc } from "firebase/firestore"; 
 import {db} from "../firebase-config"
 import Update from './Update';
 
@@ -8,11 +8,12 @@ const FirstDataGet = () => {
     const [users,setUsers] = useState();
     const [name,setName] = useState("")
     const [age,setAge] = useState()
+    const [gander,setGender] = useState("male")
     const [show,setShow] = useState(false)
     const [dataId,setDataId] = useState(null)
     const conntion = collection(db,"users")
 
-
+ 
 
     const getData = async()=>{
       const data = await getDocs(conntion) 
@@ -38,12 +39,23 @@ const FirstDataGet = () => {
       setShow(true)
     }
 
+    const deleteData = async(id)=>{
+        const docsItem = doc(db,"users",id)
+        alert("are you delete data")
+        const data = await deleteDoc(docsItem)
+        console.log(data)
+    }
+
 
   return (
     <div className='max-w-[700px] m-auto pt-4'>
         <div className='flex flex-col items-center gap-2'>
             <input type="text" placeholder='Enter Name' onChange={(e)=>setName(e.target.value)} className='py-1 px-2 rounded-md border border-gray-500' />
             <input type="number" placeholder='Enter Age' onChange={(e)=>setAge(e.target.value)} className='py-1 px-2 rounded-md border border-gray-500' />
+            <select onChange={(e)=>setGender(e.target.value)} className='py-1 px-2 rounded-md border border-gray-500'>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
             <button onClick={()=>creactUser()} className='py-1 px-2 rounded-md border border-gray-500'>save</button>
         </div>
             <div className='flex items-center flex-col gap-5 justify-center'>
@@ -55,11 +67,11 @@ const FirstDataGet = () => {
                 <p>{item.age}</p>
                 <div>
                   <button onClick={()=> addataId(item.id)} className='py-1 px-2 rounded-md bg-green-500 text-white'>update</button>
-                  <button className='py-1 px-2 rounded-md bg-red-500 text-white'>delete</button>
+                  <button onClick={()=>deleteData(item.id)} className='py-1 px-2 rounded-md bg-red-500 text-white'>delete</button>
                 </div>
 
                 {
-                  show && <Update id={dataId}/>
+                  show && <Update id={dataId} setShow={setShow}/>
                 }
               </div>
             )
--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs,addDoc,doc, query, where, orderBy } from "firebase/firestore"; 
+import {doc, getDoc, updateDoc, } from "firebase/firestore"; 
 import {db} from "../firebase-config"
 
-const Update = ({id}) => {
+const Update = ({id,setShow}) => {
 
     const [name,setName] = useState("")
     const [age,setAge] = useState()
@@ -12,26 +12,44 @@ const Update = ({id}) => {
   
       useEffect(() => {
         const getData = async()=>{
-          
+          const docRef = doc(db, "users", id);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data().name);
+            setName(docSnap.data().name)
+            setAge(docSnap.data().age)
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+
+
          }
           getData()
       }, [id])
 
 
 
-    const updateUser = ()=>{
-
+    const updateUser = async()=>{
+      const docRef = doc(db, "users", id);
+       await updateDoc(docRef, {name,age});
+       setShow(false)
     }
 
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-slate-400 flex items-center justify-center '>
         <div className='w-[400px] h-[400px] bg-white flex items-center justify-center'>
         <div className='flex flex-col items-center gap-2'>
-            <input type="text" placeholder='Enter Name' onChange={(e)=>setName(e.target.value)} className='py-1 px-2 rounded-md border border-gray-500' />
-            <input type="number" placeholder='Enter Age' onChange={(e)=>setAge(e.target.value)} className='py-1 px-2 rounded-md border border-gray-500' />
+            <input type="text" placeholder='Enter Name' value={name} onChange={(e)=>setName(e.target.value)} className='py-1 px-2 rounded-md border border-gray-500' />
+            <input type="text" placeholder='Enter Age' value={age} onChange={(e)=>setAge(e.target.value)} className='py-1 px-2 rounded-md border border-gray-500' />
             <button onClick={()=>updateUser()} className='py-1 px-2 rounded-md border border-gray-500'>save</button>
+            <button className='py-1 px-2 rounded-md bg-red-600 text-white border border-gray-500' onClick={()=>setShow(false)}>close</button>
         </div>
+        
         </div>
+
+        
     </div>
   )
 }
