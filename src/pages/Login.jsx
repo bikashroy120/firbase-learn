0906706a569023cfc6e2628df782e784f336f051
bikashroy120 <bikashroydt@gmail.com>
2023-03-06 17:33:02 +0600

    add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {FcGoogle} from "react-icons/fc"
 
 const Login = () => {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+
   return (
     <div className=' h-screen w-full bg-slate-300 pt-[135px]'>
         <div className="container w-full h-full">
             <div className='w-full h-full flex items-center justify-center'>
-                <div className='w-[500px] h-[500px] bg-white shadow-lg rounded-lg p-5'>
+                <div className='w-[450px] h-auto bg-white shadow-lg rounded-lg p-5'>
                     <h2 className='text-center text-[25px] font-bold py-5'>Log In</h2>
                     <div className='flex items-center flex-col w-full gap-4'>
-                        <input type="text" placeholder='Enter Email' className='w-full py-2 border border-gray-400 rounded-lg px-2' />
-                        <input type="text" placeholder='Enter Email' className='w-full py-2 border border-gray-400 rounded-lg px-2' />
-                        <button className='w-full py-2 border border-gray-400 rounded-lg'>Log in</button>
+                        <input type="text" placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)} className='w-full py-2 border border-gray-400 rounded-lg px-2' />
+                        <input type="password" placeholder='Enter password' value={password} onChange={(e)=>setPassword(e.target.value)} className='w-full py-2 border border-gray-400 rounded-lg px-2' />
+                        <button className='w-full py-2 border border-gray-400 bg-blue-500 text-white rounded-lg'>Log in</button>
                     </div>
-                    <Link to={"/"}></Link>
+                    <Link className=' border-b border-gray-600' to={"/forget"}>Forget pasword?</Link>
+
+                    <div className='flex items-center justify-center gap-2 py-4'>
+                        <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                        <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                            <span>OR</span>
+                        <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                        <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                    </div>
+                     <button className='w-full flex items-center justify-center gap-2 py-2 border border-gray-400 bg-orange-600 text-white rounded-lg'><FcGoogle/> Login With Google</button>
+                     <div className='text-center mt-3'>
+                        <p>Dont have a account?<Link className='font-bold' to={"/signup"}>Signup</Link></p>
+                     </div>
                 </div>
             </div>
         </div>
--- src/pages/Regester.jsx
+++ src/pages/Regester.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase-config';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
-const Regester = () => {
+const Regster = () => {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [compassword,setComPassword] = useState("");
+    const [lodding,setLodding] = useState(false)
+    const navigate =  useNavigate()
+
+    const submitRegester = ()=>{
+      setLodding(true)
+      if(password!==compassword){
+        toast.error("Password didnt macth")
+        setLodding(false)
+      }else{
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user)
+          setLodding(false)
+          toast.success("Regester successful...")
+          navigate("/login")
+        })
+        .catch((error) => {
+          setLodding(false)
+          toast.error(error.message)
+        });
+      }
+    }
+
+
   return (
-    <div>Regester</div>
+    <>
+        {
+          lodding && <Loader/>
+        }
+
+        <div className=' h-screen w-full bg-slate-300 pt-[135px]'>
+          <div className="container w-full h-full">
+              <div className='w-full h-full flex items-center justify-center'>
+                  <div className='w-[450px] h-auto bg-white shadow-lg rounded-lg p-5'>
+                      <h2 className='text-center text-[25px] font-bold py-5'>Sign Up</h2>
+                      <div className='flex items-center flex-col w-full gap-4'>
+                          <input type="text" placeholder='Enter Email' value={email} onChange={(e)=>setEmail(e.target.value)} className='w-full py-2 border border-gray-400 rounded-lg px-2' />
+                          <input type="password" placeholder='Enter password' value={password} onChange={(e)=>setPassword(e.target.value)} className='w-full py-2 border border-gray-400 rounded-lg px-2' />
+                          <input type="password" placeholder='Comfrom password' value={compassword} onChange={(e)=>setComPassword(e.target.value)} className='w-full py-2 border border-gray-400 rounded-lg px-2' />
+                          <button onClick={submitRegester} className='w-full py-2 border border-gray-400 bg-blue-500 text-white rounded-lg'>Log in</button>
+                      </div>
+                    
+
+                      <div className='flex items-center justify-center gap-2 py-4'>
+                          <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                          <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                              <span>OR</span>
+                          <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                          <div className=' bg-gray-500 h-[2px] w-[15px]'></div>
+                      </div>
+        
+                      <div className='text-center mt-3'>
+                          <p>You have a account?<Link className='font-bold' to={"/login"}>Login</Link></p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+    </>
   )
 }
 
-export default Regester
+export default Regster
